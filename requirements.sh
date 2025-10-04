7f89d4ae13675c7db63118f5648b309c90c43205
Mansoor Barri <mansoor.eb.ak@gmail.com>
2025-10-04 16:32:40 +0000

    add more fonts

--- requirements.sh
+++ requirements.sh
@@ -6,6 +6,8 @@ pacman -S base-devel git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ..
+rm -rf yay
 
 # enable sddm 
 sudo systemctl enable sddm
@@ -23,6 +25,7 @@ systemctl --user start pipewire pipewire-pulse wireplumber
 pacman -S pavucontrol --noconfirm 
 
 # fonts
-pacman -S ttf-dejavu ttf-font-awesome noto-fonts
-
-
+pacman -S ttf-dejavu ttf-font-awesome noto-fonts --noconfirm
+sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra --no-confirm
+yay -S nerd-fonts-complete --noconfirm 
+sudo fc-cache -fv
